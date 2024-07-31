e5204dba6815492cce540a2229c669e65c5b462e
Austin Fraser <austinbfraser@gmail.com>
2024-07-31 09:09:26 -0700

    setup for jest

---
+++ __tests__/auth.js
@@ -0,0 +1,16 @@
+const path = require('path');
+
+/*
+flows to test:
+1. sign up
+2. sign in
+3. log out
+4. update fname
+5. update lname
+6. update email address
+7. update pw
+8. update avatar
+
+need to set up test user db!
+
+*/
